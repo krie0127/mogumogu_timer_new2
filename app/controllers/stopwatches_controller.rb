ee9575eb00f573eed34b964164e515618c46bafd
your name <your-email@example.com>
2023-12-15 07:57:15 +0900

    ストップウォッチ機能実装

---
+++ app/controllers/stopwatches_controller.rb
@@ -0,0 +1,46 @@
+class StopwatchesController < ApplicationController
+
+  def create
+    hours = params[:hours]
+    minutes = params[:minutes]
+    seconds = params[:seconds] 
+    
+    @time = Stopwatch.new(stopwatch_params)
+    
+    if @time.save
+      render json: { status: 'success', message: 'Time saved successfully' }
+    else
+      render json: { status: 'error', message: 'Failed to save time' }
+    end
+
+    end
+    
+    def index
+      @times = Stopwatch.all
+      respond_to do |format|
+      format.html
+      format.json { render json: @times }
+    end
+      end
+    
+    def update
+      @time = Stopwatch.find(params[:id]) # 特定のタイムレコードを取得
+    
+      if @time.update(stopwatch_params)
+        redirect_to stopwatches_path, notice: 'Time record was successfully updated.'
+      else
+        render :edit
+      end
+    end
+  
+    private
+  
+    def set_stopwatch
+      @stopwatch = Stopwatch.find(params[:id])
+    end
+  
+    def stopwatch_params
+      params.require(:stopwatch).permit(:hours, :minutes, :seconds) 
+    end
+  end
+  
